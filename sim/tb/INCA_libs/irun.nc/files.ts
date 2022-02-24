44cfeeb8eea9b044b0a6416aa4e0a9a31afb4320
Arthur Kassick Ferreira <akferreira@inf.ufrgs.br>
2022-02-24 20:17:52 -0300

    Adiciona código assembly ao testbench

--- sim/tb/INCA_libs/irun.nc/files.ts
+++ sim/tb/INCA_libs/irun.nc/files.ts
@@ -1,2 +1,2 @@
 1635659280 /home/inf01185/arthur.ferreira/work/cpu_z80/rtl/cpu_z80.v
-1636113047 /home/inf01185/arthur.ferreira/work/cpu_z80/sim/tb/tb_z80.v
+1637387648 /home/inf01185/arthur.ferreira/work/cpu_z80/sim/tb/tb_z80.v
--- sim/tb/tb_z80.v
+++ sim/tb/tb_z80.v
@@ -1,16 +1,22 @@
 
 module tb_z80();
-	wire [7:0] DI;
+	reg [7:0] DI;
 	reg CLK, RESET, INT, NMI, WAIT;
+        reg [7:0] mem1 [0:7];
 	wire [7:0] DO;
 	wire [15:0] ADDR;
 	wire WR, MREQ, IORQ, HALT, M1;
+	integer count;
 	
 	always begin
 		CLK = 0;
 		#5;
 		CLK = 1;
 		#5;
+		DI = mem1[count];
+		count = count + 1;
+		if(count == 7) count = 0;
+		
 	end
 
 cpu_z80 uut(.DI(DI), .DO(DO),
@@ -19,9 +25,20 @@ cpu_z80 uut(.DI(DI), .DO(DO),
 .INT(INT), .NMI(NMI), .WAIT(WAIT) );
 
 initial begin
+
+        mem1[0] = 8'h3e;
+	mem1[1] = 8'h0f;
+	mem1[2] = 8'h06;
+	mem1[3] = 8'h07;
+	mem1[4] = 8'h80;
+	mem1[5] = 8'hd3;
+	mem1[6] = 8'h76;
+
+	count = 0;
+	
 	RESET = 1; NMI = 0; INT = 0;
 	WAIT = 0;
-	#5
+	#1
 	RESET = 0;
 
 	end
